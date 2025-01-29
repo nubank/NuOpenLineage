79c0d1fec68b5461a4e0cb99e0b58e2f8f612593
Luis Yamada <34309295+lmassaoy@users.noreply.github.com>
2025-01-29 09:07:21 -0300

    Updates in spark-agent build process (#15)
    * removing all dependencies from shadowJar
    
    * Including custom shadowJar (without ext dependencies)
    
    We're adding a custom task to the `build.gradle` file to allow a customized shadowJar build (uber JAR) that does not contain external dependencies such as org.apache.http, etc.
    
    * adding the artifact of the custom shadowJar to the publishing task
    
    * including spark-extension-entrypoint in dependencies sh
    
    * enabling dependencies in pom for interfaces
    
    * remove slim shadow from build.gradle
    
    ---------
    
    Co-authored-by: Jhonatas Rosendo <jhonatas.rosendo@nubank.com.br>

--- integration/spark/buildDependencies.sh
+++ integration/spark/buildDependencies.sh
@@ -3,3 +3,4 @@ set -x -e
 (cd ../../client/java && ./gradlew publishToMavenLocal)
 (cd ../spark-extension-interfaces && ./gradlew publishToMavenLocal)
 (cd ../sql/iface-java && ./script/compile.sh && ./script/build.sh)
+(cd ../spark-extension-entrypoint && ./gradlew publishToMavenLocal)
