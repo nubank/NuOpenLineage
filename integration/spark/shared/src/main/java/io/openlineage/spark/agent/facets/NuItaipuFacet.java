44ebf0d4da01b52dad29eb7d0e8d8eb23e4f9f36
Jhonatas Rosendo <41453495+jrosend@users.noreply.github.com>
2025-03-11 14:48:15 -0300

    [DGD-4776] - Fix lineage properties overwrites (#19)
    * Renaming NuFacet to NuDefaultFacet
    
    * Creating generic class NuRunFacet
    
    * NuDefaultFacet extends from NuRunFacet
    
    * Creating NuItaipuFacet and adding method to NuFacetUtils
    
    * Changing NUFacetBuilder to consider lineage namespace and fixing tests accordingly
    
    * Reversing resolved inputs and outputs key and velue on NuItaipuFacet. dataset path is now the key and dataset name is now the value

---
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/facets/NuItaipuFacet.java
@@ -0,0 +1,34 @@
+package io.openlineage.spark.agent.facets;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.openlineage.client.OpenLineage;
+import io.openlineage.spark.agent.Versions;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Map;
+
+import static io.openlineage.spark.agent.util.NuFacetsUtils.getConfigValues;
+import static io.openlineage.spark.agent.util.NuFacetsUtils.keyToValue;
+
+@Getter
+@Slf4j
+public class NuItaipuFacet extends NuRunFacet{
+
+    private static final String RESOLVED_INPUTS_PREFIX = "spark.nuOpenLineage.inputPath.";
+    private static final String RESOLVED_OUTPUTS_PREFIX = "spark.nuOpenLineage.outputPath.";
+
+    @JsonProperty("resolvedInputs")
+    private Map<String, String> resolvedInputs;
+
+    @JsonProperty("resolvedOutputs")
+    private Map<String, String> resolvedOutputs;
+
+    public NuItaipuFacet(@NonNull SparkSession sparkSession) {
+        super(Versions.OPEN_LINEAGE_PRODUCER_URI);
+        this.resolvedInputs = keyToValue(getConfigValues(RESOLVED_INPUTS_PREFIX, sparkSession.conf()));
+        this.resolvedOutputs = keyToValue(getConfigValues(RESOLVED_OUTPUTS_PREFIX, sparkSession.conf()));
+    }
+}
